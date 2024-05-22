b98415721068fbea24b38e5e9341e610ea926796
HuabeiYou <huabeiyou@gmail.com>
2024-05-22 11:08:16 -0400

    updates

--- lua/user/plugins/indentline.lua
+++ lua/user/plugins/indentline.lua
@@ -2,7 +2,12 @@ return {
   "lukas-reineke/indent-blankline.nvim",
   main = "ibl",
   opts = {
+    indent = {
+      char = "▏", -- This is a slightly thinner char than the default one, check :help ibl.config.indent.char
+    },
     scope = {
+      show_start = false,
+      show_end = false,
       highlight = {
         "rainbow2",
         "rainbow3",
