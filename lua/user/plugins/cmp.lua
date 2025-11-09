d2501cd606b5d896e2e014064e4891940a203670
HuabeiYou <huabeiyou@gmail.com>
2025-11-09 10:41:44 +0800

    update

--- lua/user/plugins/cmp.lua
+++ lua/user/plugins/cmp.lua
@@ -1,112 +1,136 @@
 ---@diagnostic disable: missing-fields
 local M = {
-  "hrsh7th/nvim-cmp",
-  lazy = false,
-  priority = 100,
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/cmp-calc",
-    "onsails/lspkind.nvim",
-    {
-      "L3MON4D3/LuaSnip",
-      version = "v2.*",
-      build = "make install_jsregexp",
-      dependencies = {
-        "rafamadriz/friendly-snippets",
-        "molleweide/LuaSnip-snippets.nvim",
+  {
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+      require("supermaven-nvim").setup({
+        keymaps = {
+          accept_suggestion = "<Tab>",
+          clear_suggestion = "<C-]>",
+          accept_word = "<C-j>",
+        },
+        ignore_filetypes = { cpp = true }, -- or { "cpp", }
+        -- color = {
+        --   suggestion_color = "#ffffff",
+        --   cterm = 244,
+        -- },
+        log_level = "off", -- set to "off" to disable logging completely
+        disable_inline_completion = false, -- disables inline completion for use with cmp
+        disable_keymaps = false, -- disables built in keymaps for more manual control
+        condition = function()
+          return false
+        end, -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
+      })
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    lazy = false,
+    priority = 100,
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/cmp-calc",
+      "onsails/lspkind.nvim",
+      {
+        "L3MON4D3/LuaSnip",
+        version = "v2.*",
+        build = "make install_jsregexp",
+        dependencies = {
+          "rafamadriz/friendly-snippets",
+          "molleweide/LuaSnip-snippets.nvim",
+        },
       },
+      "saadparwaiz1/cmp_luasnip",
     },
-    "saadparwaiz1/cmp_luasnip",
+    config = function()
+      local cmp = require("cmp")
+
+      local luasnip = require("luasnip")
+      luasnip.filetype_extend("javascript", { "jsdoc" })
+      require("luasnip.loaders.from_vscode").lazy_load()
+
+      local lspkind = require("lspkind")
+      lspkind.init({})
+
+      cmp.setup({
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" }, -- For luasnip users.
+        }, {
+          { name = "buffer" },
+          { name = "path" },
+          { name = "calc" },
+        }),
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+          end,
+        },
+        mapping = {
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-y>"] = cmp.mapping(
+            cmp.mapping.confirm({
+              behavior = cmp.SelectBehavior.Insert,
+              select = true,
+            }),
+            { "i", "c" }
+          ),
+          ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+          ["<C-k>"] = cmp.mapping(function()
+            if not cmp.visible_docs() then
+              cmp.open_docs()
+            end
+            if cmp.visible_docs() then
+              cmp.close_docs()
+            end
+          end, { "i", "c" }),
+          ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+          ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+          -- <C-l> will move you to the right of each of the expansion locations.
+          -- <C-h> is similar, except moving you backwards.
+          ["<C-l>"] = cmp.mapping(function()
+            if luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            end
+          end, { "i", "s" }),
+          ["<C-h>"] = cmp.mapping(function()
+            if luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            end
+          end, { "i", "s" }),
+        },
+        window = {
+          completion = {
+            winblend = 0,
+          },
+        },
+      })
+
+      cmp.setup.filetype({
+        "sql",
+        "mysql",
+      }, {
+        sources = cmp.config.sources({
+          { name = "vim-dadbod-completion" },
+          { name = "buffer" },
+        }),
+      })
+      cmp.setup.filetype({
+        "org",
+        "markdown",
+      }, {
+        sources = cmp.config.sources({
+          { name = "orgmode" },
+          { name = "buffer" },
+          { name = "nvim_lsp" },
+        }),
+      })
+    end,
   },
 }
-
-function M.config()
-  local cmp = require("cmp")
-
-  local luasnip = require("luasnip")
-  luasnip.filetype_extend("javascript", { "jsdoc" })
-  require("luasnip.loaders.from_vscode").lazy_load()
-
-  local lspkind = require("lspkind")
-  lspkind.init({})
-
-  cmp.setup({
-    sources = cmp.config.sources({
-      { name = "nvim_lsp" },
-      { name = "luasnip" }, -- For luasnip users.
-    }, {
-      { name = "buffer" },
-      { name = "path" },
-      { name = "calc" },
-    }),
-    snippet = {
-      expand = function(args)
-        require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-      end,
-    },
-    mapping = {
-      ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-      ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-      ["<C-y>"] = cmp.mapping(
-        cmp.mapping.confirm({
-          behavior = cmp.SelectBehavior.Insert,
-          select = true,
-        }),
-        { "i", "c" }
-      ),
-      ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-      ["<C-k>"] = cmp.mapping(function()
-        if not cmp.visible_docs() then
-          cmp.open_docs()
-        end
-        if cmp.visible_docs() then
-          cmp.close_docs()
-        end
-      end, { "i", "c" }),
-      ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-      ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-      -- <C-l> will move you to the right of each of the expansion locations.
-      -- <C-h> is similar, except moving you backwards.
-      ["<C-l>"] = cmp.mapping(function()
-        if luasnip.expand_or_jumpable() then
-          luasnip.expand_or_jump()
-        end
-      end, { "i", "s" }),
-      ["<C-h>"] = cmp.mapping(function()
-        if luasnip.jumpable(-1) then
-          luasnip.jump(-1)
-        end
-      end, { "i", "s" }),
-    },
-    window = {
-      completion = {
-        winblend = 0,
-      },
-    },
-  })
-
-  cmp.setup.filetype({
-    "sql",
-    "mysql",
-  }, {
-    sources = cmp.config.sources({
-      { name = "vim-dadbod-completion" },
-      { name = "buffer" },
-    }),
-  })
-  cmp.setup.filetype({
-    "org",
-    "markdown",
-  }, {
-    sources = cmp.config.sources({
-      { name = "orgmode" },
-      { name = "buffer" },
-      { name = "nvim_lsp" },
-    }),
-  })
-end
 
 return M
--- lua/user/plugins/toggleterm.lua
+++ lua/user/plugins/toggleterm.lua
@@ -20,7 +20,7 @@ function M.config()
     start_in_insert = true,
     insert_mappings = true, -- whether or not the open mapping applies in insert mode
     persist_size = false,
-    direction = "float",
+    direction = "horizontal",
     close_on_exit = true, -- close the terminal window when the process exits
     shell = vim.o.shell,
     float_opts = {
@@ -35,6 +35,8 @@ function M.config()
     },
   })
 
+  local opts = { noremap = true, silent = true }
+
   local lazygit = require("toggleterm.terminal").Terminal:new({
     cmd = "lazygit",
     dir = "git_dir",
@@ -42,7 +44,7 @@ function M.config()
     -- function to run on opening the terminal
     on_open = function(term)
       vim.cmd("startinsert!")
-      vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+      vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", opts)
     end,
     -- function to run on closing the terminal
     on_close = function(term)
@@ -54,7 +56,7 @@ function M.config()
     lazygit:toggle()
   end
 
-  vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua _lazygit_toggle()<CR>", opts)
 end
 
 return M
