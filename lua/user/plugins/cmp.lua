8f7cafc1d261bbca7c8a9fc5d847b6571dc19cad
HuabeiYou <huabeiyou@gmail.com>
2025-11-26 08:18:18 +0800

    try copilot

--- lua/user/plugins/cmp.lua
+++ lua/user/plugins/cmp.lua
@@ -1,28 +1,34 @@
 ---@diagnostic disable: missing-fields
 local M = {
   {
-    "supermaven-inc/supermaven-nvim",
+    "github/copilot.vim",
     config = function()
-      require("supermaven-nvim").setup({
-        keymaps = {
-          accept_suggestion = "<Tab>",
-          clear_suggestion = "<C-]>",
-          accept_word = "<C-j>",
-        },
-        ignore_filetypes = { cpp = true }, -- or { "cpp", }
-        -- color = {
-        --   suggestion_color = "#ffffff",
-        --   cterm = 244,
-        -- },
-        log_level = "off", -- set to "off" to disable logging completely
-        disable_inline_completion = false, -- disables inline completion for use with cmp
-        disable_keymaps = false, -- disables built in keymaps for more manual control
-        condition = function()
-          return false
-        end, -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
-      })
+      vim.g.copilot_node_command = vim.fn.expand("/opt/homebrew/bin/node")
     end,
   },
+  -- {
+  --   "supermaven-inc/supermaven-nvim",
+  --   config = function()
+  --     require("supermaven-nvim").setup({
+  --       keymaps = {
+  --         accept_suggestion = "<Tab>",
+  --         clear_suggestion = "<C-]>",
+  --         accept_word = "<C-j>",
+  --       },
+  --       ignore_filetypes = { cpp = true }, -- or { "cpp", }
+  --       -- color = {
+  --       --   suggestion_color = "#ffffff",
+  --       --   cterm = 244,
+  --       -- },
+  --       log_level = "off", -- set to "off" to disable logging completely
+  --       disable_inline_completion = false, -- disables inline completion for use with cmp
+  --       disable_keymaps = false, -- disables built in keymaps for more manual control
+  --       condition = function()
+  --         return false
+  --       end, -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
+  --     })
+  --   end,
+  -- },
   {
     "hrsh7th/nvim-cmp",
     lazy = false,
