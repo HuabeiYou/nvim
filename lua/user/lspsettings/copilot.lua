5d87ca00b41d97e5ab6dacb4049099cdba74f641
HuabeiYou <huabeiyou@gmail.com>
2026-06-08 00:20:04 +0000

    fix(copilot): pin language server to node 24

--- lua/user/lspsettings/copilot.lua
+++ lua/user/lspsettings/copilot.lua
@@ -1,4 +1,4 @@
-local node_path = os.getenv("HOME") .. "/Library/Application Support/fnm/node-versions/v22.21.1/installation/bin/node"
+local node_path = os.getenv("HOME") .. "/Library/Application Support/fnm/node-versions/v24.16.0/installation/bin/node"
 local copilot_server_path = vim.fn.stdpath("data")
   .. "/mason/packages/copilot-language-server/node_modules/.bin/copilot-language-server"
 return {
--- lua/user/plugins/copilot.lua
+++ lua/user/plugins/copilot.lua
@@ -10,16 +10,11 @@ return {
   },
   opts = function(_, opts)
     local node_path = os.getenv("HOME")
-      .. "/Library/Application Support/fnm/node-versions/v22.21.1/installation/bin/node"
+      .. "/Library/Application Support/fnm/node-versions/v24.16.0/installation/bin/node"
     local copilot_server_path = vim.fn.stdpath("data")
       .. "/mason/packages/copilot-language-server/node_modules/.bin/copilot-language-server"
     return {
       server_opts_overrides = {
-        -- cmd = {
-        --   node_path,
-        --   copilot_server_path,
-        --   "--stdio",
-        -- },
         settings = {
           telemetry = {
             telemetryLevel = "off",
